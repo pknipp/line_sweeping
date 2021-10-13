f7049f5f9fdbfabfe6dc90115401e68d3154fd6b
Peter Knipp <peteraknipp@gmail.com>
2021-10-13 14:27:43 -0400

    Translate facToItin from js to py

--- line_sweeping/facToItin.py
+++ line_sweeping/facToItin.py
@@ -1,23 +1,21 @@
-const facToItin = (n, iter) => {
-    let facPerm = 1;
-    for (let i = 1; i <= n; i++) facPerm *= i;
-    let itin = [];
-    // range = [0, 1, 2, ...], which is used to generate permutations
-    let range = new Array(n).fill(0).map((blah, i) => i);
-    let fac = facPerm;
-    for(let place = n - 1; place >= 0; place --){
-      let i = n - 1 - place;
-      fac /= (place + 1);
-      let digit = Math.floor(iter/fac);
-      let index = range.splice(digit,1)[0];
-      itin.push(index);
-      iter -= digit * fac;
-    }
-    return itin;
-}
+import math
 
-// for (let i = 0; i < 24; i++) {
-//     console.log(facToItin(4, i));
-// }
+def facToItin(n, iter):
+  facPerm = 1
+  for i in range(1, n + 1):
+    facPerm *= i
+    itin = []
+    integers = range(n)
+    fac = facPerm
+    for place in reversed(range(n)):
+      i = n - 1 - place
+      fac /= (place + 1)
+      digit = math.floor(iter/fac)
+      index = integers[digit]
+      del integers[digit]
+      itin.append(index)
+      iter -= digit * fac
+    return itin
 
-module.exports = facToItin;
+# for i in range(0, 24):
+#   print facToItin(4, i)
